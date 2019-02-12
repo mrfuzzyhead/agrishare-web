b5a4eda1686205bcabf1179c993b7a28cf9cd657
Brad Searle <brad@c2.co.zw>
2019-02-12 15:32:29 +0200

    API to get current app version

---
+++ API/Controllers/App/ConfigController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Entities = Agrishare.Core.Entities;
+
+namespace Agrishare.API.Controllers.App
+{
+    public class ConfigController : BaseApiController
+    {
+        [Route("config/currentversion")]
+        [AcceptVerbs("GET")]
+        public object CurrentVersion()
+        {
+            return Success(new
+            {
+                Version = Entities.Config.Find(Key: "App Version")?.Value ?? "1.0.0"
+            });
+        }
+    }
+}
