47841584c123ec272b1e65d40c812a0fbb25d43a
Brad Searle <brad@c2.co.zw>
2020-08-11 11:54:51 +1000

    Settlement report download Don't log "operation cancelled" Try/catch DOB on web User CurrentUser in BasePage

---
+++ CMS/Code/BasePage.cs
@@ -0,0 +1,37 @@
+﻿using Agrishare.Core.Entities;
+
+namespace Agrishare.CMS.Code
+{
+    public class BasePage : System.Web.UI.Page
+    {
+        public User CurrentUser
+        {
+            get
+            {
+                if (currentUser == null)
+                {
+                    try
+                    {
+                        string encryptedToken = Request.Cookies[Core.Entities.User.AuthCookieName].Value;
+                        string authToken = Core.Utils.Encryption.DecryptWithRC4(encryptedToken, Config.EncryptionSalt);
+                        currentUser = Core.Entities.User.Find(AuthToken: authToken);
+                    }
+                    catch
+                    {
+                        currentUser = new User();
+                    }
+
+                    if (currentUser == null)
+                        currentUser = new User();
+                }
+
+                return currentUser;
+            }
+            set
+            {
+                currentUser = value;
+            }
+        }
+        private User currentUser;
+    }
+}
